308bf0f634e729d884b4b552dcd161a88d1bf599
pig-cabbage <2089319261@qq.com>
2019-04-29 17:11:28 +0800

    Initial Commit

---
+++ pages/ClockStart/ClockStart.js
@@ -0,0 +1,96 @@
+// pages/ClockStart/ClockStart.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    intervarID: '', //定时器名字
+    minutes: 0,
+    seconds: 0,
+    countDownNum: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //设置今天打卡时间
+  goal_of_time: function (e) {
+    this.setData({
+      minutes: e.detail.value
+
+
+    })
+  },
+  //开始打卡
+  clock_start: function () {
+    var that = this;
+    var sum = that.data.minutes * 60;
+    this.data.intervarID = setInterval(function () {
+      if (sum == 0) {
+
+        clearInterval(that.data.intervarID);
+      }
+      let m = Math.floor(sum / 60);
+      let s = sum % 60;
+      sum--;
+      that.setData({
+        minutes: m,
+        seconds: s
+      })
+    }, 1000)
+  }
+
+})
