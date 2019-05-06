fc827409c5743d446e7bf711ef42520196c8f391
pig-cabbage <2089319261@qq.com>
2019-05-06 18:46:48 +0800

    pig-cabbage

---
+++ pages/ClockSearch/ClockSearch.js
@@ -0,0 +1,71 @@
+var app = getApp()
+
+// pages/ClockSearch/ClockSearch.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    items: [
+      { groupName: "Clock1", groupTag: "运动", groupDuration: "100min", groupDays: "300天", groupMember: "400" },
+      { groupName: "Clock2", groupTag: "学习", groupDuration: "10min", groupDays: "7天", groupMember: "10" }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
