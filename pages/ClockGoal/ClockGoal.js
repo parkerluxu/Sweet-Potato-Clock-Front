19e5f70edb1ced5c06540d7caefa9fe5f6b66c2e
pig-cabbage <2089319261@qq.com>
2019-05-06 16:32:35 +0800

    pig-cabbage

--- pages/ClockGoal/ClockGoal.js
+++ pages/ClockGoal/ClockGoal.js
@@ -8,120 +8,120 @@ Page({
     inputVal: "",
     tag: ["学习", "运动", "工作", "自定义"],
     tagIndex: 0,
-    hiddenName:false,
+    hiddenName: false,
     reply: false,
     duration: "00:10",
     unit: ["天", "周", "月"],
     unitIndex: 0
   },
 
-  inputBind: function(e) {
+  inputBind: function (e) {
     this.setData({
       inputVal: e.detail.value
     });
   },
 
-  query: function() {
+  query: function () {
 
   },
-  bindTagChange: function(e) {
+  bindTagChange: function (e) {
     console.log('选择', this.data.tag[e.detail.value])
     if (e.detail.value == 3) {
       this.setData({
         reply: true,
-        hiddenName:true
+        hiddenName: true
       })
     } else {
       this.setData({
         reply: false,
-        
+
       })
-  }
-  this.setData({
-    tagIndex: e.detail.value
-  })
-},
+    }
+    this.setData({
+      tagIndex: e.detail.value
+    })
+  },
 
-bindDurationChange: function(e) {
-  this.setData({
-    duration: e.detail.value
-  })
-},
+  bindDurationChange: function (e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
 
-bindUnitChange: function(e) {
-  console.log('picker发生选择改变，携带值为', e.detail.value);
+  bindUnitChange: function (e) {
+    console.log('picker发生选择改变，携带值为', e.detail.value);
 
-  this.setData({
-    unitIndex: e.detail.value
-  })
-},
+    this.setData({
+      unitIndex: e.detail.value
+    })
+  },
 
-/**
- * 生命周期函数--监听页面加载
- */
-onLoad: function(options) {
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
 
-},
+  },
 
-/**
- * 生命周期函数--监听页面初次渲染完成
- */
-onReady: function() {
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
 
-},
+  },
 
-/**
- * 生命周期函数--监听页面显示
- */
-onShow: function() {
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
 
-},
+  },
 
-/**
- * 生命周期函数--监听页面隐藏
- */
-onHide: function() {
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
 
-},
+  },
 
-/**
- * 生命周期函数--监听页面卸载
- */
-onUnload: function() {
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
 
-},
+  },
 
-/**
- * 页面相关事件处理函数--监听用户下拉动作
- */
-onPullDownRefresh: function() {
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
 
-},
+  },
 
-/**
- * 页面上拉触底事件的处理函数
- */
-onReachBottom: function() {
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
 
-},
+  },
 
-/**
- * 用户点击右上角分享
- */
-onShareAppMessage: function() {
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
 
-},
+  },
 
 
-clock_search:function(){
-  wx.navigateTo({
-    url: '../clockSearch/clockSearch',
-  })
-},
+  clock_search: function () {
+    wx.navigateTo({
+      url: '../clockSearch/clockSearch',
+    })
+  },
 
   group_detail: function () {
     wx.navigateTo({
-      url: '../groupDetail/groupDetail',
+      url: '../GroupDetail/GroupDetail',
     })
   }
 })
