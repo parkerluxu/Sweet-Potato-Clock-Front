18ab9ab1e5c8dbda568febaa1ca639f7ede2c4c9
pig-cabbage <2089319261@qq.com>
2019-05-08 19:21:09 +0800

    pig-cabbage

--- pages/MyForest/MyForest.js
+++ pages/MyForest/MyForest.js
@@ -2,69 +2,70 @@
 
 Page({
   data: {
-    number:0,
-    area:0,
-    sum:0,
-    
+    number: 0,
+    area: 0,
+    sum: 0,
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-   
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
-  success_buy:function(){
+  success_buy: function() {
     var that = this;
+
     wx.request({
       url: 'http://127.0.0.1:8080/buytrees/buytrees',
       method: 'GET',
@@ -72,32 +73,36 @@ Page({
         userId: wx.getStorageSync('openid'),
         number: that.data.number
       },
-      success:function(res){
-        if(res.data.success==1){
-          var toastText="购买成功";
-          wx.showToast({
-            title: toastText,
-            icon:"success",
-            duration:2000
-          })
-        }else{
-          var toastText = "积分不足";
-          wx.showToast({
-            title: toastText,
-            image: '../images/关闭.png',
-            icon:'loading',
-            duration: 2000
-          })
+      success: function(res) {
+        if (this.data.number == 0) {
+
+        } else {
+          if (res.data.success == 1) {
+            var toastText = "购买成功";
+            wx.showToast({
+              title: toastText,
+              icon: "success",
+              duration: 2000
+            })
+          } else {
+            var toastText = "积分不足";
+            wx.showToast({
+              title: toastText,
+              image: '../images/关闭.png',
+              icon: 'loading',
+              duration: 2000
+            })
+          }
         }
       }
     })
   },
-  showScore:function(e){
-    
+  showScore: function(e) {
+
     this.setData({
-      number:e.detail.value,
-     
-  })
+      number: e.detail.value,
+
+    })
   }
 
 })
