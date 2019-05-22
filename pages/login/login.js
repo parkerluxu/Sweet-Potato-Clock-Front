aa25ea2b07bc125dbb5a006fec2d120f0c004cff
lxpk <503682014@qq.com>
2019-05-22 09:47:02 +0800

    record联调成功
    record联调成功

---
+++ pages/login/login.js
@@ -0,0 +1,186 @@
+var app = getApp();
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    logged: false
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+  bindGetUserInfo: function (event) {
+    console.log(event.detail.userInfo)
+    var that=this;
+    wx.getSetting({
+      success: res => {
+        if (!res.authSetting['scope.userInfo']){
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success(res) {
+              console.log(res.scope.userInfo)
+            }
+          })
+          wx.login({
+            success: function (r) {
+              //获取临时凭证
+              var code = r.code;
+              wx.getUserInfo({
+                success: function (res) {
+                  console.log({
+                    encryptedData: res.encryptedData,
+                    iv: res.iv,
+                    code: code
+                  })
+                  //调用后端
+                  wx.request({
+                    url: 'http://localhost:8080/WXLogin',
+                    data: {
+                      encryptedData: res.encryptedData,
+                      iv: res.iv,
+                      code: code,
+                    },
+                    method: "POST",
+                    success: function (result) {
+                      console.log(result.data)
+                      if (result.data.status == 1) {
+                        console.log(result.data.userInfo.openId);
+                        app.globalData.userInfo = result.data.userInfo;
+                        wx.setStorageSync('logged', true);
+                        wx.setStorageSync('openid', result.data.userInfo.openId);
+                      } else {
+                        console.log('解密失败')
+                      }
+                      wx.switchTab({
+                        url: '../record/record',
+                      })
+                    },
+                    fail: function () {
+                      console.log("系统错误")
+                    }
+                  })
+                }
+              })
+            }
+          })
+        }
+        if (res.authSetting['scope.userInfo']) {
+          wx.login({
+            success: function (r) {
+              //获取临时凭证
+              var code = r.code;
+              wx.getUserInfo({
+                success: function (res) {
+                  console.log({
+                    encryptedData: res.encryptedData,
+                    iv: res.iv,
+                    code: code
+                  })
+                  //调用后端
+                  wx.request({
+                    url: 'http://localhost:8080/WXLogin',
+                    data: {
+                      encryptedData: res.encryptedData,
+                      iv: res.iv,
+                      code: code,
+                    },
+                    method: "POST",
+                    success: function (result) {
+                      if (result.data.status == 1) {
+                        console.log(result.data.userInfo.openId);
+                        app.globalData.userInfo = result.data.userInfo;
+                        wx.setStorageSync('logged', true);
+                        wx.setStorageSync('openid', result.data.userInfo.openId);
+                      } else {
+                        console.log('解密失败')
+                      }
+                      wx.switchTab({
+                        url: '../record/record',
+                      })
+                    },
+                    fail: function () {
+                      console.log("系统错误")
+                    }
+                  })
+                }
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+
+  login: function (e) { 
+    wx.authorize({
+      scope: 'scope.userInfo',
+      success(res) {
+        console.log(res.scope.userInfo)
+      }
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that = this
+    that.setData({
+      logged: wx.getStorageSync('logged')
+    })
+    if (that.data.logged == true) {
+      setTimeout(function () {
+        wx.switchTab({
+          url: '../record/record',
+        })
+      }, 2000)
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
--- pages/record/record.js
+++ pages/record/record.js
@@ -1,54 +1,76 @@
 // pages/record/record.js
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    axis: [
-      {
-        date: '2018-2-15',
-        time: '15:28',
-        name: '张三',
-        event: '垃圾太多'
-      },
-      {
-        date: '2018-2-15',
-        time: '15:28',
-        name: '王三',
-        event: '垃圾太多'
-      },
-      {
-        date: '2018-2-15',
-        time: '15:28',
-        name: '张三',
-        event: '垃圾太多'
-      },
-      {
-        date: '2018-2-15',
-        time: '15:28',
-        name: '张三',
-        event: '垃圾太多'
-      },
+    axis: [{
+      date: '2018-2-15',
+      time: '15:28',
+      name: '张三',
+      event: '垃圾太多'
+    },
+    {
+      date: '2018-2-15',
+      time: '15:28',
+      name: '王三',
+      event: '垃圾太多'
+    },
+    {
+      date: '2018-2-15',
+      time: '15:28',
+      name: '张三',
+      event: '垃圾太多'
+    },
+    {
+      date: '2018-2-15',
+      time: '15:28',
+      name: '张三',
+      event: '垃圾太多'
+    },
     ],
-     selected: [
-      {
-        date: '2018-5-21'
-      }, {
-        date: '2018-5-22'
-      },{
-        date: '2018-5-24'
-      },{
-        date: '2018-5-25'
-      }
-    ]
+    recordInfo: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    var nowDate = new Date();
+    var year = nowDate.getFullYear();
+    var month = nowDate.getMonth() + 1 < 10 ? ("0" + (nowDate.getMonth() + 1)) : nowDate.getMonth() + 1;
+    var day = nowDate.getDate() < 10 ? ("0" + nowDate.getDate()) : nowDate.getDate();
+    var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+    var minutes = nowDate.getMinutes() < 10? "0" + nowDate.getMinutes() : nowDate.getMinutes();
+    var seconds = nowDate.getSeconds() < 10? "0" + nowDate.getSeconds() : nowDate.getSeconds();
+    var dateStr = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+    console.log(dateStr);
+    wx.request({
+      url: 'http://localhost:8080/displayrecord',
+      method: 'GET',
+      data: {
+        userid: wx.getStorageSync('openid'),
+        date: dateStr,
+      },
+      success: function (res) {
+        console.log(res.data)
+        var recordList = res.data.recordList;
+        var goalList = res.data.goalList;
+        for (var i = 0; i < recordList.length; i++) {
+          var dateTime = 'recordInfo[' + i + '].dateTime'
+          var minutes = 'recordInfo[' + i + '].minutes'
+          var goal = 'recordInfo[' + i + '].content'
+          that.setData({
+            [dateTime]: recordList[i].dateTime,
+            [minutes]: recordList[i].minutes,
+            [goal]: goalList[i].content,
+          })
+        }
+      }
+    })
   },
 
   /**
@@ -108,6 +130,5 @@ Page({
   bindgetdate(e) {
     let time = e.detail;
     console.log(time)
-
   }
 })
