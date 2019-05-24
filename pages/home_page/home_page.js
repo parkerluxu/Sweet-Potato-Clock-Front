f45df68d3c08273b344de1f46305aed8cb31403e
lxpk <503682014@qq.com>
2019-05-24 23:07:00 +0800

    增加了一些功能
    增加了一些功能

--- pages/home_page/home_page.js
+++ pages/home_page/home_page.js
@@ -6,13 +6,15 @@ Page({
    */
   data: {
     clock: [{
-      clockMode: "专注",
       clockName: "考研加油还有一年",
-      clockTime: "30min"
+      clockTime: "30min",
+      isComplete:Boolean,
+      goalId: Number,
     }],
     plan: [{
-      planMode: "计划",
-      planName: "按钮？"
+      isComplete: Boolean,
+      planName: "按钮？",
+      goalId:Number,
     }],
     dates: [{
         name: "Sun",
@@ -65,13 +67,13 @@ Page({
     hiddenDate: true,
     goal: {
       minutes: Number,
-      isConcentrate:Boolean,
+      isConcentrate: Boolean,
     },
-    disposable:false,
-    goalId:Number,
+    disposable: false,
+    goalId: Number,
   },
 
-//选择“一次”、“每天”、“自定义”
+  //选择“一次”、“每天”、“自定义”
   selectOnce: function(e) {
     var that = this;
     let index = e.currentTarget.dataset.index;
@@ -87,16 +89,16 @@ Page({
       //如果选择一次性，则把goalDate中的dispasable设为true
       if (index == 1) {
         that.setData({
-          disposable:true
+          disposable: true
         })
         console.log(that.data.disposable)
       }
       //如果选择每天，则把周一到周日都设为true
-      if(index == 2){
-        for(var i=0;i<that.data.dates.length;i++){
-          var dateSel='dates['+i+'].selected'
+      if (index == 2) {
+        for (var i = 0; i < that.data.dates.length; i++) {
+          var dateSel = 'dates[' + i + '].selected'
           that.setData({
-            [dateSel]:true,
+            [dateSel]: true,
           })
         }
         console.log(that.data.dates)
@@ -107,7 +109,7 @@ Page({
     })
 
   },
-//设置打卡周期
+  //设置打卡周期
   selectDate: function(e) {
     let index = e.currentTarget.dataset.index;
     let arrs = this.data.dates;
@@ -128,8 +130,8 @@ Page({
     this.util(currentStatu)
   },
 
-//确认，发送请求添加数据
-  addGoal:function(e){
+  //确认，发送请求添加数据
+  addGoal: function(e) {
     var that = this;
     that.setData({
       goalId: null,
@@ -144,10 +146,10 @@ Page({
         concentrated: that.data.goal.isConcentrate,
         minutes: that.data.goal.minutes,
       },
-      success: function (res) {
+      success: function(res) {
         console.log(res.data)
         that.setData({
-          goalId:res.data.goalId,
+          goalId: res.data.goalId,
         })
         wx.request({
           url: 'http://localhost:8080/goaldate/addgoaldate',
@@ -162,11 +164,14 @@ Page({
             friday: that.data.dates[5].selected,
             saturday: that.data.dates[6].selected,
             disposable: that.data.disposable,
+          },
+          success: function (res) {
+            that.onShow()
           }
         })
+        
       },
     })
-   
     that.drawer(e);
   },
 
@@ -214,7 +219,7 @@ Page({
       });
     }
   },
-//选择了专注模式
+  //选择了专注模式
   selectC: function() {
     var that = this;
     that.setData({
@@ -230,7 +235,7 @@ Page({
     })
     console.log(that.data.goal.isConcentrate)
   },
-//选择了计划模式
+  //选择了计划模式
   selectP: function() {
     var that = this;
     that.setData({
@@ -248,7 +253,7 @@ Page({
     console.log(that.data.goal.isConcentrate)
   },
 
-//设置目标名称
+  //设置目标名称
   setGoalName: function(e) {
     console.log(e.detail.value)
     var that = this
@@ -257,14 +262,14 @@ Page({
     })
   },
 
-//设置目标时间
+  //设置目标时间
   setGoalMinutes: function(e) {
     var that = this
     console.log(e.detail.value)
     var minutes = parseInt(e.detail.value)
-      that.setData({
-        'goal.minutes': minutes,
-      })
+    that.setData({
+      'goal.minutes': minutes,
+    })
   },
 
   hidebtn: function() {
@@ -291,14 +296,87 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    var that = this;
+    wx.request({
+      url: 'http://localhost:8080/displaygoal/displaygoal',
+      method: 'GET',
+      data: {
+        userId: wx.getStorageSync('openid')
+      },
+      success: function(res) {
+        console.log(res.data.goalList)
+        var goalList = res.data.goalList;
+        var clockNum = 0
+        var planNum = 0;
+        for (var i = 0; i < goalList.length; i++) {
+          var clockName = 'clock[' + clockNum + '].clockName'
+          var clockId = 'clock[' + clockNum + '].goalId'
+          var clockTime = 'clock[' + clockNum + '].clockTime'
+          var clockComplete = 'clock[' + clockNum + '].isComplete'
+          var planName = 'plan[' + planNum + '].planName'
+          var planComplete = 'plan[' + planNum + '].isComplete'
+          var planId = 'plan[' + planNum + '].goalId'
+          if (goalList[i].concentrated == true) {
+            that.setData({
+              [clockName]: goalList[i].content,
+              [clockTime]: goalList[i].minutes,
+              [clockComplete]: goalList[i].complete,
+              [clockId]: goalList[i].goalId,
+            })
+            clockNum+=1
+          }else{
+            that.setData({
+              [planName]: goalList[i].content,
+              [planComplete]: goalList[i].complete,
+              [planId]: goalList[i].goalId,
+            })
+            planNum+=1
+          }
+        }
+      }
+    })
   },
 
+  finishPlan:function(e){
+    var that=this;
+    console.log(e.currentTarget.dataset['goalid'])
+    var goalId = e.currentTarget.dataset['goalid']
+    wx.request({
+      url: 'http://localhost:8080/record/planComplete',
+      method: 'POST',
+      data: {
+        userId: wx.getStorageSync('openid'),
+        goalId: goalId,
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.onShow()
+      }
+    })
+  },
+
+  unFinishPlan:function(e){
+    var that = this;
+    console.log(e.currentTarget.dataset['goalid'])
+    var goalId = e.currentTarget.dataset['goalid']
+    wx.request({
+      url: 'http://localhost:8080/record/planUnComplete',
+      method: 'POST',
+      data: {
+        userId: wx.getStorageSync('openid'),
+        goalId: goalId,
+      },
+      success: function (res) {
+        console.log(res.data)
+        that.onShow()
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function() {
-
+    
   },
 
   /**
