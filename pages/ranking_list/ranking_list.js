9fb43c91c69e721fda172212ab3a9e62fde3f6b7
pig-cabbage <2089319261@qq.com>
2019-05-26 01:46:55 +0800

    pig-cabbage

--- pages/ranking_list/ranking_list.js
+++ pages/ranking_list/ranking_list.js
@@ -26,10 +26,13 @@ Page({
     groupName: String,
     text: String,
     showModalStatus: false,
-    userId: "2",
+    userId: "1",
     isAbled: true,
     group: null,
-    isPrivate:false
+    isPrivate:false,
+    startX: 0, //开始坐标
+
+    startY: 0
 
 
   },
@@ -115,11 +118,14 @@ Page({
           var k2 = 'showList[' + i + '].name';
           var k3 = 'showList[' + i + '].minutesSum';
           var k4 = 'showList[' + i + '].minutes';
+          var k5 = 'showList[' + i +'].isTouchMove';
+          var k6='showList['+i+
           that.setData({
             [k1]: that.data.memberList[i].avatar,
             [k2]: that.data.memberList[i].userNickname,
             [k3]: that.data.memberList[i].minutesSum,
-            [k4]: that.data.timeList[i]
+            [k4]: that.data.timeList[i],
+            [k5]:false,
           })
         }
 
@@ -275,7 +281,127 @@ Page({
       }
 
   })
-  }
+  },
+  //手指触摸动作开始 记录起点X坐标
 
+  touchstart: function (e) {
+    var that = this;
+    if (that.data.userId == that.data.captainId) {
+
+    //开始触摸时 重置所有删除
+
+    this.data.showList.forEach(function (v, i) {
+
+      if (v.isTouchMove)//只操作为true的
+
+        v.isTouchMove = false;
+
+    })
+
+    this.setData({
+
+      startX: e.changedTouches[0].clientX,
+
+      startY: e.changedTouches[0].clientY,
+
+      showList: this.data.showList
+
+    })
+    }
+
+  },
+
+  //滑动事件处理
+
+  touchmove: function (e) {
+    var that=this;
+    if(that.data.userId==that.data.captainId){
+    var that = this,
+
+      index = e.currentTarget.dataset.index,//当前索引
+
+      startX = that.data.startX,//开始X坐标
+
+      startY = that.data.startY,//开始Y坐标
+
+      touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+
+      touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+
+      //获取滑动角度
+
+      angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+
+    that.data.showList.forEach(function (v, i) {
+
+      v.isTouchMove = false
+
+      //滑动超过30度角 return
+
+      if (Math.abs(angle) > 30) return;
+
+      if (i == index) {
+
+        if (touchMoveX > startX) //右滑
+
+          v.isTouchMove = false
+
+        else {//左滑
+
+          v.isTouchMove = true
+          
+        }
+
+      }
+
+    })
+
+    //更新数据
+
+    that.setData({
+
+      showList: that.data.showList
+
+    })
+    }
+  },
+
+  /**
+  
+  * 计算滑动角度
+  
+  * @param {Object} start 起点坐标
+  
+  * @param {Object} end 终点坐标
+  
+  */
+
+  angle: function (start, end) {
+
+    var _X = end.X - start.X,
+
+      _Y = end.Y - start.Y
+
+    //返回角度 /Math.atan()返回数字的反正切值
+
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+
+  },
+
+  //删除事件
+
+  del: function (e) {
+    var that=this;
+
+
+    this.data.showList.splice(e.currentTarget.dataset.index, 1)
+
+    this.setData({
+
+      showList: this.data.showList
+
+    })
+
+  }
 
 })
