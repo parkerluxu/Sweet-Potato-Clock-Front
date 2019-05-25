bcec17cb5d083b6bc048c8a193035cb4005ac8ac
lxpk <503682014@qq.com>
2019-05-25 10:59:52 +0800

    完成了每日按时间统计的功能
    完成了每日按时间统计的功能

--- pages/my_forest/my_forest.js
+++ pages/my_forest/my_forest.js
@@ -2,12 +2,7 @@
 var wxCharts = require('../../utils/wxcharts.js');
 var app = getApp();
 var columnChart = null;
-var chartData = {
-  main: {
-    data: [0, 20, 30, 40, 300, 30, 10, 20, 30, 40, 100, 30, 0, 20, 30, 40, 300, 30, 10, 20, 30, 40, 100, 30],
-    categories: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-  }
-};
+
 Page({
 
   /**
@@ -27,41 +22,19 @@ Page({
     status: [],
     datastr: [],
     tree: [],
+    chartData : {
+      main: {
+        data: [0, 20, 30, 40, 300, 30, 10, 20, 30, 40, 100, 30, 0, 20, 30, 40, 300, 30, 10, 20, 30, 40, 100, 30],
+        categories: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
+      }
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that=this;
-    var that = this;
-    var nowDate = new Date();
-    var year = nowDate.getFullYear();
-    var month = nowDate.getMonth() + 1 < 10 ? ("0" + (nowDate.getMonth() + 1)) : nowDate.getMonth() + 1;
-    var day = nowDate.getDate() < 10 ? ("0" + nowDate.getDate()) : nowDate.getDate();
-    var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
-    var minutes = nowDate.getMinutes() < 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
-    var seconds = nowDate.getSeconds() < 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
-    var dateStr = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
-    console.log(dateStr);
-    that.setData({
-      'dateStr.year': year,
-      'dateStr.month':month,
-      'dateStr.day':day,
-    })
-    wx.request({
-      url: 'http://localhost:8080/userinformation/userinformation',
-      data:{
-        userid:wx.getStorageSync('openid')
-      },
-      method:'GET',
-      success:function(res){
-        console.log(res.data)
-        that.setData({
-          tree: res.data.treeNumber,
-        })
-      }
-    })
+   
   },
 
   /**
@@ -75,43 +48,93 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    try {
-      var width = wx.getSystemInfoSync().windowWidth;
-    } catch (e) {
-      console.error('getSystemInfoSync failed!');
-    }
-    columnChart = new wxCharts({
-      canvasId: 'columnCanvas',
-      type: 'column',
-      animation: true,
-      categories: chartData.main.categories,
-      series: [{
-        name: "打卡时长",
-        color: '#fa9857',
-        data: chartData.main.data,
-        format: function (val, name) {
-          return val.toFixed(0);
+    var that=this
+    var nowDate = new Date();
+    var year = nowDate.getFullYear();
+    var month = nowDate.getMonth() + 1 < 10 ? ("0" + (nowDate.getMonth() + 1)) : nowDate.getMonth() + 1;
+    var day = nowDate.getDate() < 10 ? ("0" + nowDate.getDate()) : nowDate.getDate();
+    var hour = nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+    var minutes = nowDate.getMinutes() < 10 ? "0" + nowDate.getMinutes() : nowDate.getMinutes();
+    var seconds = nowDate.getSeconds() < 10 ? "0" + nowDate.getSeconds() : nowDate.getSeconds();
+    var dateStr = year + "-" + month + "-" + day;
+    var userid = wx.getStorageSync('openid')
+    console.log(dateStr);
+    that.setData({
+      'dateStr.year': year,
+      'dateStr.month': month,
+      'dateStr.day': day,
+    })
+    wx.request({
+      url: 'http://localhost:8080/userinformation/userinformation',
+      data: {
+        userid: wx.getStorageSync('openid')
+      },
+      method: 'GET',
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          tree: res.data.treeNumber,
+        })
+      }
+    })
+    console.log(userid)
+    wx.request({
+      url: 'http://localhost:8080/displayuserstastic/displayuserstasticday',
+      method: 'GET',
+      data: {
+        userId: userid,
+        date: dateStr,
+      },
+      success: function (res) {
+        that.setData({
+          'chartData.main.data': res.data.minutesList
+        })
+        console.log(res.data)
+        console.log(that.data.chartData.main.data)
+        try {
+          var width = wx.getSystemInfoSync().windowWidth;
+        } catch (e) {
+          console.error('getSystemInfoSync failed!');
         }
-      }],
-      yAxis: {
-        format: function (val) {
-          return val + '分钟';
-        },
-        min: 0
+        columnChart = new wxCharts({
+          canvasId: 'columnCanvas',
+          type: 'column',
+          animation: true,
+          categories: that.data.chartData.main.categories,
+          legend:false,
+          series: [{
+            name: "打卡时长",
+            color: '#fa9857',
+            data: that.data.chartData.main.data,
+            format: function (val, name) {
+              return val.toFixed(0);
+            },
+            
+          }],
+          yAxis: {
+            format: function (val) {
+              return val + '分钟';
+            },
+            min: 0,
+            disabled:true,
+            max:120
+          },
+          xAxis: {
+            disableGrid: false,
+            type: 'calibration'
+          },
+          extra: {
+            column: {
+              width: 15,
+            },
+            legendTextColor: '#000000'
+          },
+          width: width,
+          height: 150,
+        });
       },
-      xAxis: {
-        disableGrid: false,
-        type: 'calibration'
-      },
-      extra: {
-        column: {
-          width: 15,
-        },
-        legendTextColor: '#000000'
-      },
-      width: width,
-      height: 150,
-    });
+    })
+    
   },
 
   /**
