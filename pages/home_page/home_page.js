8e63c7b5a742ff4142916392d088ece598c2f54e
pig-cabbage <2089319261@qq.com>
2019-05-27 16:15:32 +0800

    pig-cabbage

--- pages/home_page/home_page.js
+++ pages/home_page/home_page.js
@@ -86,6 +86,9 @@ Page({
     },
     disposable: false,
     goalId: Number,
+    startX: 0, //开始坐标
+
+    startY: 0
   },
 
   //选择“一次”、“每天”、“自定义”
@@ -466,6 +469,120 @@ Page({
   clockStart: function() {
     wx.navigateTo({
       url: '../clock/clock'
+    })
+  },
+  //手指触摸动作开始 记录起点X坐标
+
+  touchstart: function (e) {
+    var that = this;
+    if (that.data.userId == that.data.captainId) {
+
+      //开始触摸时 重置所有删除
+
+      this.data.clock_1.forEach(function (v, i) {
+
+        if (v.isTouchMove)//只操作为true的
+
+          v.isTouchMove = false;
+
+      })
+
+      this.setData({
+
+        startX: e.changedTouches[0].clientX,
+
+        startY: e.changedTouches[0].clientY,
+
+        clock_1: this.data.clock_1
+
+      })
+    }
+
+  },
+
+  //滑动事件处理
+
+  touchmove: function (e) {
+    var that = this;
+    if (that.data.userId == that.data.captainId) {
+      var that = this,
+
+        index = e.currentTarget.dataset.index,//当前索引
+
+        startX = that.data.startX,//开始X坐标
+
+        startY = that.data.startY,//开始Y坐标
+
+        touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
+
+        touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
+
+        //获取滑动角度
+
+        angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
+
+      that.data.clock_1.forEach(function (v, i) {
+
+        v.isTouchMove = false
+
+        //滑动超过30度角 return
+
+        if (Math.abs(angle) > 30) return;
+
+        if (i == index) {
+
+          if (touchMoveX > startX) //右滑
+
+            v.isTouchMove = false
+
+          else {//左滑
+
+            v.isTouchMove = true
+
+          }
+
+        }
+
+      })
+
+      //更新数据
+
+      that.setData({
+
+        clock_1: that.data.clock_1
+
+      })
+    }
+  },
+
+  /**
+  
+  * 计算滑动角度
+  
+  * @param {Object} start 起点坐标
+  
+  * @param {Object} end 终点坐标
+  
+  */
+
+  angle: function (start, end) {
+
+    var _X = end.X - start.X,
+
+      _Y = end.Y - start.Y
+
+    //返回角度 /Math.atan()返回数字的反正切值
+
+    return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
+
+  },
+  del: function (e) {
+    this.data.clock_1.splice(e.currentTarget.dataset.index, 1)
+
+    this.setData({
+
+      clock_1: this.data.clock_1
+
     })
   }
 })
