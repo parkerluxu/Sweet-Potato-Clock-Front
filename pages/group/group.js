a88795ef5251202ace19b9e946b264346fe94c78
unknown <unknown@example.com>
2019-05-28 01:49:17 +0800

    小组页

--- pages/group/group.js
+++ pages/group/group.js
@@ -37,7 +37,7 @@ Page({
       })
     }
   },
-
+  //新建小组
   //弹窗
   powerDrawer: function(e) {
     var currentStatu = e.currentTarget.dataset.statu;
@@ -87,11 +87,57 @@ Page({
       });
     }
   },
-  //新建小组
-  transpond: function() {
-    console.log("transpond")
-  },
+  
   //加入小组
+  powerDrawer1: function (e) {
+    var currentStatu = e.currentTarget.dataset.statu;
+    this.util1(currentStatu)
+  },
+  util1: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例 
+    var animation = wx.createAnimation({
+      duration: 200, //动画时长
+      timingFunction: "linear", //线性
+      delay: 0 //0则不延迟
+    });
+
+    // 第2步：这个动画实例赋给当前的动画实例
+    this.animation = animation;
+
+    // 第3步：执行第一组动画
+    animation.opacity(0).rotateX(-100).step();
+
+    // 第4步：导出动画对象赋给数据对象储存
+    this.setData({
+      animationData: animation.export()
+    })
+
+    // 第5步：设置定时器到指定时候后，执行第二组动画
+    setTimeout(function () {
+      // 执行第二组动画
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
+      this.setData({
+        animationData: animation
+      })
+
+      //关闭
+      if (currentStatu == "close") {
+        this.setData({
+          showModalStatus: false
+        });
+      }
+    }.bind(this), 200)
+
+    // 显示
+    if (currentStatu == "open") {
+      this.setData({
+        showModalStatus: true
+      });
+    }
+  },
+
    //选择了是
   selectC: function () {
     var that = this;
@@ -146,8 +192,8 @@ Page({
       timingFunction: 'ease-out'
     })
     animationPlus.rotateZ(180).step();
-    animationcollect.translate(0, -150).rotateZ(360).opacity(1).step();
-    animationTranspond.translate(0, -75).rotateZ(360).opacity(1).step();
+    animationcollect.translate(-20, -80).rotateZ(360).opacity(1).step();
+    animationTranspond.translate(-80, -20).rotateZ(360).opacity(1).step();
     animationInput.translate(-100, 100).rotateZ(180).opacity(1).step();
     this.setData({
       animPlus: animationPlus.export(),
